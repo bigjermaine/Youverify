e5a458ebafcf446610a4fa62750d2a071e11bee1
bigjermaine <danieljermaine1@gmail.com>
2025-03-13 03:51:08 +0100

    Initial Commit

---
+++ Youverify/YouverifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouverifyApp.swift
+//  Youverify
+//
+//  Created by Daniel Jermaine on 13/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouverifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
